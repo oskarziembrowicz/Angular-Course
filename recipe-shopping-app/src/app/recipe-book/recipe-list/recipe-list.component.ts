c6bc9777d4d26d008295474fd0a5daaa05223f16
oskarziembrowicz <rakson2002@gmail.com>
2023-05-20 13:39:43 +0200

    Adding for loop insode of recipe-list to display multiple recipes

--- recipe-shopping-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ recipe-shopping-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is just a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png')
+    new Recipe('A test recipe', 'This is just a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png'),
+    new Recipe('A second test recipe', 'This is a second example', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png')
   ];
 }
