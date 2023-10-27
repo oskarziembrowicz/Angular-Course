55ea7bde3e0ad6e140eff2b1a2dc6b869ac0f4ef
oskarziembrowicz <rakson2002@gmail.com>
2023-10-27 16:18:13 +0200

    In the mainn app adding the option to display recipes

--- recipe-shopping-app/src/app/recipe-book/recipe-book.component.ts
+++ recipe-shopping-app/src/app/recipe-book/recipe-book.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-book',
   templateUrl: './recipe-book.component.html',
-  styleUrls: ['./recipe-book.component.css']
+  styleUrls: ['./recipe-book.component.css'],
 })
 export class RecipeBookComponent {
-
+  selectedRecipe: Recipe;
 }
--- recipe-shopping-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ recipe-shopping-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is just a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png'),
-    new Recipe('A second test recipe', 'This is a second example', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png')
+    new Recipe(
+      'A test recipe',
+      'This is just a test',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png'
+    ),
+    new Recipe(
+      'A second test recipe',
+      'This is a second example',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png'
+    ),
   ];
+
+  onRecipeSelected(selectedRecipe: Recipe) {
+    this.recipeWasSelected.emit(selectedRecipe);
+  }
 }
