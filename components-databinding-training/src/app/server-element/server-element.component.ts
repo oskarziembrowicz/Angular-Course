ae8697631269f1225df6b5d2a5d23c791975b6e6
oskarziembrowicz <rakson2002@gmail.com>
2023-10-27 14:11:23 +0200

    Learning about @ContentChild

--- components-databinding-training/src/app/server-element/server-element.component.ts
+++ components-databinding-training/src/app/server-element/server-element.component.ts
@@ -12,7 +12,8 @@ import {
   AfterViewChecked,
   OnDestroy,
   ViewChild,
-  ElementRef
+  ElementRef,
+  ContentChild
 } from '@angular/core';
 
 @Component({
@@ -33,6 +34,7 @@ export class ServerElementComponent implements
   @Input('srvElement') element: {type: string, name: string, content: string};
   @Input() name: string;
   @ViewChild("heading", {static: true}) header: ElementRef;
+  @ContentChild("contentParagraph", {static: true}) paragraph: ElementRef;
 
   constructor() {
     console.log("constructor called!");
@@ -46,6 +48,7 @@ export class ServerElementComponent implements
   ngOnInit() {
     console.log("ngOnInit called!");
     console.log("Text content: " + this.header.nativeElement.textContent);
+    console.log("Text content of paragraph: " + this.paragraph.nativeElement.textContent);
   }
 
   ngDoCheck() {
@@ -54,6 +57,7 @@ export class ServerElementComponent implements
 
   ngAfterContentInit() {
     console.log("ngAfterContentInit called!");
+    console.log("Text content of paragraph: " + this.paragraph.nativeElement.textContent);
   }
 
   ngAfterContentChecked() {
