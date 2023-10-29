b32ac942b6efdfabcc13222fabecf3bd7feacfa2
oskarziembrowicz <rakson2002@gmail.com>
2023-10-29 13:45:35 +0100

    Changing appearence of elements on mouse hover using HostListener

--- directives-section/src/app/better-highlight/better-highlight.directive.ts
+++ directives-section/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
@@ -7,6 +13,18 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+  }
+
+  @HostListener('mouseenter') mouseOver(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'backgroundColor',
+      'transparent'
+    );
   }
 }
