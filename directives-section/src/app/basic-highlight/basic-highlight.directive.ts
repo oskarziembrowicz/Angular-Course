916986a5e82f773b6e08f94c3c9f58670c827bd5
oskarziembrowicz <rakson2002@gmail.com>
2023-10-29 13:22:04 +0100

    Creating own directive: BasicHighlightDirective

---
+++ directives-section/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appBasicHighlight]',
+})
+export class BasicHighlightDirective implements OnInit {
+  constructor(private elementReference: ElementRef) {} // constructor automatically assigns to newly created variable: elementReference
+
+  ngOnInit() {
+    this.elementReference.nativeElement.style.backgroundColor = 'green';
+  }
+}
