3b22c26520b48d1dcce10b530710a3bfb81611d9
oskarziembrowicz <rakson2002@gmail.com>
2023-10-29 15:12:09 +0100

    Making dropdowns work better

--- recipe-shopping-app/src/app/shared/dropdown.directive.ts
+++ recipe-shopping-app/src/app/shared/dropdown.directive.ts
@@ -1,19 +1,22 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  HostBinding,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
-  isOpen = false;
+  @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleDropdown(eventData: Event) {
-    if (this.isOpen) {
-      this.renderer.removeClass(this.elementRef.nativeElement, 'open');
-      this.isOpen = false;
-    } else {
-      this.renderer.addClass(this.elementRef.nativeElement, 'open');
-      this.isOpen = true;
-    }
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event']) toggleDropdown(event: Event) {
+    this.isOpen = this.elementRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
 }
