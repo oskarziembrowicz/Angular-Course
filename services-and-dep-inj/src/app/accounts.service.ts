d8dbebdb384d33ac467e9a0ead539bfde4fc9757
oskarziembrowicz <rakson2002@gmail.com>
2023-11-09 14:38:29 +0100

    Enclosing logging functionality inside of the accounts Service by injecting Service into a Service

--- services-and-dep-inj/src/app/accounts.service.ts
+++ services-and-dep-inj/src/app/accounts.service.ts
@@ -1,3 +1,7 @@
+import { Injectable } from '@angular/core';
+import { LoggingService } from './logging.service';
+
+@Injectable()
 export class AccountsService {
   accounts = [
     {
@@ -14,11 +18,15 @@ export class AccountsService {
     },
   ];
 
+  constructor(private loggingService: LoggingService) {}
+
   addAccount(name: string, status: string) {
     this.accounts.push({ name, status });
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
