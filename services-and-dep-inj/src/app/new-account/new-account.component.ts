ba974e8ec9c79df7ef390d979b6d0de97ecc86b6
oskarziembrowicz <rakson2002@gmail.com>
2023-11-09 13:31:24 +0100

    Creating new service: LoggingService

--- services-and-dep-inj/src/app/new-account/new-account.component.ts
+++ services-and-dep-inj/src/app/new-account/new-account.component.ts
@@ -3,16 +3,15 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
-      status: accountStatus
+      status: accountStatus,
     });
-    console.log('A server status changed, new status: ' + accountStatus);
   }
 }
