761c1de822472de699a544320086980007356831
oskarziembrowicz <rakson2002@gmail.com>
2023-11-22 14:43:05 +0100

    Passing ingredients list to component on change via event subscribtion

--- recipe-shopping-app/src/app/shopping-list/shopping-list.service.ts
+++ recipe-shopping-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,9 @@
+import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
-  // Manage list ingredients
-  // Add ingredients
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -14,5 +15,6 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
