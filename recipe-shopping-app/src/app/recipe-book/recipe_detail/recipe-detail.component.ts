1f6a4297ae5a10f1449b187f603c0d70c82e957b
oskarziembrowicz <rakson2002@gmail.com>
2023-11-22 15:32:51 +0100

    Sending ingredients from a recipe to shopping list

--- recipe-shopping-app/src/app/recipe-book/recipe_detail/recipe-detail.component.ts
+++ recipe-shopping-app/src/app/recipe-book/recipe_detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,4 +10,13 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent {
   @Input('recipeForDetails') recipe: Recipe;
+
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  onAddIngredientsToShoppingList() {
+    // for (let ingredient of this.recipe.ingredients) {
+    //   this.shoppingListService.addIngredient(ingredient);
+    // }
+    this.shoppingListService.addIngredients(this.recipe.ingredients);
+  }
 }
