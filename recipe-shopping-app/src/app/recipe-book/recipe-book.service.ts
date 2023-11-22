b220fc3ea55b5a9d2e7a1ada24be7c400d1ffbab
oskarziembrowicz <rakson2002@gmail.com>
2023-11-22 15:18:49 +0100

    Printing ingredients in the recipe-detail

--- recipe-shopping-app/src/app/recipe-book/recipe-book.service.ts
+++ recipe-shopping-app/src/app/recipe-book/recipe-book.service.ts
@@ -1,19 +1,27 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeBookService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
-      'This is just a test',
-      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png'
+      'Tasty Schnitzel',
+      'A very tasty and delicious Schnitzel',
+      'https://imgs.search.brave.com/FdUVAlcoqCWt25pGvoQvx8v9W_cmayYYD_L2zvpasLo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTI5/OTU4MTk3OS9waG90/by9jaGlja2VuLXNj/aG5pdHplbC5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9ZVJn/eFFMVDlhdlA3eGVK/Y3htX25HcDE5cFlh/WHRKZlllQUg0TEE1/Vk10az0',
+      [new Ingredient('Meat', 1), new Ingredient('French fries', 20)]
     ),
     new Recipe(
-      'A second test recipe',
-      'This is a second example',
-      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png'
+      'Big Burger',
+      'Probably one of the best things in the world',
+      'https://imgs.search.brave.com/LTw0etsujNuP_Yh2xFgQruMO3Sk9MtgmDnN-uZEvMKo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNTk1/NTQ1NzA4L3Bob3Rv/L2NoZWVzZWJ1cmdl/ci5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9MUcybm5qUS1M/S0M1cm4tRFAwZEVs/aWc2bmtGNG1mdW1Z/dnpHV2x3X0huQT0',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Beef', 1),
+        new Ingredient('Tomatoe', 1),
+        new Ingredient('Salad', 1),
+      ]
     ),
   ];
 
--- recipe-shopping-app/src/app/recipe-book/recipe.model.ts
+++ recipe-shopping-app/src/app/recipe-book/recipe.model.ts
@@ -1,11 +1,20 @@
-export class Recipe {
-    public name: string;
-    public description: string;
-    public imagePath: string;
+import { Ingredient } from '../shared/ingredient.model';
 
-    constructor(name: string, description: string, imagePath: string) {
-        this.name = name;
-        this.description = description;
-        this.imagePath = imagePath;
-    }
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+  public ingredients: Ingredient[];
+
+  constructor(
+    name: string,
+    description: string,
+    imagePath: string,
+    ingredients: Ingredient[]
+  ) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+    this.ingredients = ingredients;
+  }
 }
