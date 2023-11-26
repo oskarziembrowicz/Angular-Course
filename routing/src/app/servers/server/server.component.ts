2ec30feff1b39fe3c272a6bdd058ac008a62a2a7
oskarziembrowicz <rakson2002@gmail.com>
2023-11-26 16:55:57 +0100

    Adding routes to the elements in the servers list

--- routing/src/app/servers/server/server.component.ts
+++ routing/src/app/servers/server/server.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number; name: string; status: string };
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    // id in the route is a string and it needs to be a number
+    this.server = this.serversService.getServer(
+      +this.route.snapshot.params['id']
+    );
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.server = this.serversService.getServer(+params['id']);
+    });
   }
-
 }
