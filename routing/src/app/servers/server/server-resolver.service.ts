e6371f49ab46282470c24b5b2b21b21a7a000982
oskarziembrowicz <rakson2002@gmail.com>
2023-11-27 14:41:18 +0100

    Resolving dynamic data in form of a server in the server route

---
+++ routing/src/app/servers/server/server-resolver.service.ts
@@ -0,0 +1,26 @@
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { ServersService } from '../servers.service';
+import { Injectable } from '@angular/core';
+
+interface Server {
+  id: number;
+  name: string;
+  status: string;
+}
+
+@Injectable()
+export class ServerResolver implements Resolve<Server> {
+  constructor(private serversService: ServersService) {}
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Server | Observable<Server> | Promise<Server> {
+    return this.serversService.getServer(+route.params['id']);
+  }
+}
--- routing/src/app/servers/server/server.component.ts
+++ routing/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Data, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -18,13 +18,17 @@ export class ServerComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    // id in the route is a string and it needs to be a number
-    this.server = this.serversService.getServer(
-      +this.route.snapshot.params['id']
-    );
-    this.route.params.subscribe((params: Params) => {
-      this.server = this.server = this.serversService.getServer(+params['id']);
+    this.route.data.subscribe((data: Data) => {
+      this.server = data['server'];
     });
+    // id in the route is a string and it needs to be a number
+    //-------------------------------------
+    // this.server = this.serversService.getServer(
+    //   +this.route.snapshot.params['id']
+    // );
+    // this.route.params.subscribe((params: Params) => {
+    //   this.server = this.server = this.serversService.getServer(+params['id']);
+    // });
   }
 
   onEdit() {
