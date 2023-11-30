98d24b36121f41e908c1825f8f5989cf1e11bbb9
oskarziembrowicz <rakson2002@gmail.com>
2023-11-30 17:06:13 +0100

    Adding child routes - brokem

--- recipe-shopping-app/src/app/app-routing.module.ts
+++ recipe-shopping-app/src/app/app-routing.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './recipe-book/recipe_detail/recipe-detail.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipe-book', pathMatch: 'full' },
   ,
-  { path: 'recipe-book', component: RecipeBookComponent },
+  {
+    path: 'recipe-book',
+    component: RecipeBookComponent,
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: 'recipe/:id', component: RecipeDetailComponent },
+    ],
+  },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
--- recipe-shopping-app/src/app/app.module.ts
+++ recipe-shopping-app/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
+import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { AppRoutingModule } from './app-routing.module';
     RecipeItemComponent,
     RecipeDetailComponent,
     DropdownDirective,
+    RecipeStartComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ShoppingListService],
