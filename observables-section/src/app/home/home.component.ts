10471fb35c530da1f6dd12254a4e2114cc68780f
oskarziembrowicz <rakson2002@gmail.com>
2023-12-01 14:23:01 +0100

    Handling errors and completes of own observable

--- observables-section/src/app/home/home.component.ts
+++ observables-section/src/app/home/home.component.ts
@@ -19,13 +19,28 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
         count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
-      console.log(data);
-    });
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error: Error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log('Completed!');
+      }
+    );
   }
 
   ngOnDestroy(): void {
