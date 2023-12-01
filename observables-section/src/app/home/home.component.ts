839217c815dcd1b513fc92c6136f8ad0a3205e0a
oskarziembrowicz <rakson2002@gmail.com>
2023-12-01 14:11:13 +0100

    Creating my own observable using Observable.create()

--- observables-section/src/app/home/home.component.ts
+++ observables-section/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { Subscription, interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -12,8 +12,19 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() {}
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe((count) => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
+      console.log(data);
     });
   }
 
