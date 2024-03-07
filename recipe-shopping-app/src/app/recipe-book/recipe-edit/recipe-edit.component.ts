d5284de9cc5f4827b1d0573d5c11773abc44b850
oskarziembrowicz <rakson2002@gmail.com>
2024-03-07 16:24:57 +0100

    Adding ingredients array to the form

--- recipe-shopping-app/src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ recipe-shopping-app/src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeBookService } from '../recipe-book.service';
 
@@ -26,26 +26,42 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImgPath = '';
     let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImgPath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount),
+            })
+          );
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       name: new FormControl(recipeName),
       imagePath: new FormControl(recipeImgPath),
       description: new FormControl(recipeDescription),
+      ingredients: recipeIngredients,
     });
   }
 
-  onSubmit() {
-    console.log(this.recipeForm);
+  get ingredientsControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
 }
