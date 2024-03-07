ab6d40cb65629b64db7fb96a8270166134fa103b
oskarziembrowicz <rakson2002@gmail.com>
2024-03-07 15:34:27 +0100

    Adding the clear button functionality

--- recipe-shopping-app/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ recipe-shopping-app/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -47,6 +47,11 @@ export class ShoppingListEditComponent implements OnInit, OnDestroy {
     form.reset();
   }
 
+  onClear() {
+    this.shoppingListForm.reset();
+    this.editMode = false;
+  }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
